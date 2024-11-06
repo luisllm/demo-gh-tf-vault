1479e00075b85a46cd972caf70eaef3ab8a74640
luisllm <35965134+luisllm@users.noreply.github.com>
2024-11-06 08:18:03 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -172,7 +172,7 @@ resource "aws_instance" "vault_server" {
   vpc_security_group_ids = [aws_security_group.vault_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.vault-kms-unseal.id
   #user_data              = file("userdata.tftpl")
-  user_data              = data.template_file.vault.rendered
+  user_data = data.template_file.vault.rendered
 
   tags = {
     Name = "${var.environment}-vault"
